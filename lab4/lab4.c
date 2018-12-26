89902ad0b71ac3bfa21a51b0a920da1b230a7aef
BolunZhu <1870759078@qq.com>
2018-12-26 22:29:39 +0800

    init lab4

---
+++ lab4/lab4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <pwd.h>
+
+int main(int argc ,char const *argv[])
+{
+	return 0;
+}
